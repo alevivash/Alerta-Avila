281f717cee4bba86508eb950c0ef32b02685ac79
Alejandro <alejandrovivash5@gmail.com>
2026-06-24 15:35:07 -0400

    changes

--- ejecutar_alertas.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Iniciando Sistema de Alerta Temprana...
-"C:\Users\Alejandro\anaconda3\envs\satelite_env\python.exe" "C:\Users\Alejandro\Documents\Projects\ALERTAS\main_local.py"
-pause
